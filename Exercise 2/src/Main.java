c1c3d1c5ca750907d4632f8019e01d048f831a45
Rayyan1023 <80918937+Rayyan1023@users.noreply.github.com>
2021-10-11 14:57:22 -0400

    Create Main.java

---
+++ Exercise 2/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Create a ProductDB with sample data
+        ProductDB productDB = new ProductDB();
+        //Output product test with random upc number
+        productDB.addProductInfo("UPC321654876", new Product("Test Product"));
+
+        // Initialize the CashRegister and its Scanner
+        CashRegister register = new CashRegister(productDB);
+        Scanner scanner = new Scanner(register);
+
+        scanner.setScannedUpcCode("UPC321654876");//Sets sample data to scanned code
+    }
+}
