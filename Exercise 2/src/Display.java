ddcd46a696c860ff1732d39158638b8914e0c3f9
Rayyan1023 <80918937+Rayyan1023@users.noreply.github.com>
2021-10-11 14:55:41 -0400

    Create Display.java

---
+++ Exercise 2/src/Display.java
@@ -0,0 +1,13 @@
+//Is a graphical display of scanned or manually entered item name and price
+public class Display implements ViewRegister  {
+    public void Display() {
+
+    }
+
+    //Displays some text on the screen.
+    @Override
+    public void displayProduct(Product product) {
+        //Outputs display when called
+        System.out.println("Display: The product was added "" + product.GetName() + "".");
+    }
+}
