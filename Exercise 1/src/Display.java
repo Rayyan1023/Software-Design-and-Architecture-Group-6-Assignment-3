575bb2f633d83b4e3e18842c4a0a1687ac899f21
Rayyan1023 <80918937+Rayyan1023@users.noreply.github.com>
2021-10-11 14:35:39 -0400

    Create Display.java

---
+++ Exercise 1/src/Display.java
@@ -0,0 +1,11 @@
+//Is a graphical display of scanned or manually entered item name and price
+public class Display {
+    public void Display() {
+
+    }
+
+    //Displays some text on the screen.
+    public void displayText(String text) {
+        System.out.println("Display: " + text);//Outputs display when called
+    }
+}
