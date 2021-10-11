f0e472ee5d37ac549b40b71274b78ddcb9ed6bed
Rayyan1023 <80918937+Rayyan1023@users.noreply.github.com>
2021-10-11 15:25:24 -0400

    Create Product.java

---
+++ Exercise 3/src/Product.java
@@ -0,0 +1,10 @@
+public class Product {
+    private final String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String GetName() {
+        return this.name;
+    }
