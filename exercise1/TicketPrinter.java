be06cc456bfa66de08b66e7808ff4c10e7586271
Rayyan1023 <80918937+Rayyan1023@users.noreply.github.com>
2021-10-24 19:48:54 -0400

    Create TicketPrinter.java

---
+++ exercise1/TicketPrinter.java
@@ -0,0 +1,7 @@
+//Prints on paper the scanned or manually entered item name and price
+public class TicketPrinter {
+    //Prints some text on the paper.
+    public void displayText(String text) {
+        System.out.println("TicketPrinter: " + text);
+    }
+}
