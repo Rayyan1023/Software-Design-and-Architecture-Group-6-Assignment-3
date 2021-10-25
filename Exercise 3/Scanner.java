2d9b5d5e2fb3b2ac314589ac7acfb849ebec9e92
Rayyan1023 <80918937+Rayyan1023@users.noreply.github.com>
2021-10-24 22:06:59 -0400

    Create Scanner.java

---
+++ Exercise 3/Scanner.java
@@ -0,0 +1,20 @@
+//Capture of UPC code using a bar scanner
+public class Scanner {
+    //variables
+    private String scannedUpc;
+    private final CashRegister scan;
+
+    //Creates object
+    public Scanner(CashRegister scan) {
+        this.scan = scan;
+    }
+    //Captures the UPC code read by the scanner.
+    public void ScannedUpcCode(String upc) {
+        scannedUpc = upc;
+        scan.setCurrentProductUpc(scannedUpc);
+    }
+    //Gets scanned upc
+    public String getScannedUpcCode() {
+        return scannedUpc;
+    }
+}
